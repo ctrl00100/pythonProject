99118ef0aa0a6a9211639146aef8b5406a7a6ff1
k1772714@163.com <xh1772714XH>
2022-01-14 23:24:22 +0800

    异常

--- cs7异常捕获.py
+++ cs7异常捕获.py
@@ -4,3 +4,24 @@
 # @Author : Administrator
 # @File : cs7异常捕获.py
 # @Software: PyCharm
+def work1():
+    while True:
+        try:
+            n=int(input('请输入数字:'))
+        except:
+            print('请输入正确的数字')
+            continue
+        else:
+            if n<=1:
+                print('请输入大于1的数字')
+                continue
+            nums=[]
+            for i in range(1,n+1):
+                if i%2==0:
+                    nums.append(i)
+            print('所有偶数为:',nums)
+            print('偶数个数为:',len(nums))
+            num_avg=sum(nums)/len(nums)
+            print('偶数平均值:',num_avg)
+            break
+work1()
