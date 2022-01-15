7fb8a779690f84e07b53b7ba859b868d45833e9f
k1772714@163.com <xh1772714XH>
2022-01-15 17:47:37 +0800

    test2

--- cs8test/cs8test1.py
+++ cs8test/cs8test1.py
@@ -4,3 +4,36 @@
 # @Author : Administrator
 # @File : cs8test1.py
 # @Software: PyCharm
+
+import unittest
+
+
+
+class TestMusen(unittest.TestCase):
+    @classmethod
+    def setUpClass(self) -> None:
+        print('setUpClass---------')
+
+    @classmethod
+    def tearDownClass(self) -> None:
+        print('tearDown')
+    def setUp(self)->None:
+        print('tearDownClass----------------')
+
+    def tearDown(self) -> None:
+        print('tearDown')
+
+    def test_01(self):
+        print('这是第一条测试用例')
+
+    def test_02(self):
+        print('这是第2条测试用例')
+
+    def test_03(self):
+        print('这是第3条测试用例')
+
+    def test_04(self):
+        print('这是第4条测试用例')
+
+if __name__=="__main__":
+    unittest.main()
